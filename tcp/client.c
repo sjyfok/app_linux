e205a602267f6658b72d4be8ce1139b62944dedc
song <song@song-VirtualBox.(none)>
2015-12-30 10:34:28 +0800

    add arg

---
+++ tcp/client.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <fcntl.h>
+
+
+int process_request(int fd);
+
+int main(int argc, char **argv)
+{
+	int fd;
+	if (argc != 4)
+	{
+		printf("USAGE: clinet hostip, port, filename\n");
+		return -1;
+	}
+	int port = atoi(argv[2]);
+	fd = connect_to_server(argv[1], PORT);
+	if (fd == -1)
+	{
+		return -1;
+	}
+	talk_with_server(fd);
+	close(fd);
+	return 0;
+}
+
+#define	   PACK_SIZE  4096//16384
+static char testbuf[PACK_SIZE];
+
+int talk_with_server(int fd)
+{
+	int byte_cnt;
+	time_t cur_timsec = 0, end_timsec = 0, tmp_tim;
+	int recv_byte_tol = 0;
+	int fd_wr = 0, byte_wr = 0;
+
+	fd_wr = creat("test.tar.gz", 0755);
+	if (fd_wr == -1)
+	{
+		perror("Fail to open:");
+		return -1;
+	}
+	//read client cmd
+/*	byte_cnt = write(fd, &last_tm, sizeof(int)) ;
+	if (byte_cnt != sizeof(int))
+	{
+		//cmd error
+		return -1;
+	}
+*/
+
+	while (recv_byte_tol < 1687941120)// 106754626) 
+	{
+		byte_cnt = read(fd, testbuf, PACK_SIZE);
+		if (byte_cnt == -1)
+		{
+			perror("Fail to read:");
+			break;
+		}
+		byte_wr = write(fd_wr, testbuf, byte_cnt) ;
+		if (byte_wr == -1)
+		{
+			//send error
+			perror("Fail write");
+			break;
+		} 
+		recv_byte_tol += byte_cnt;
+	}
+
+	close(fd_wr);
+	return 0;
+}
---
+++ tcp/server.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <fcntl.h>
+
+
+int process_request(int fd);
+
+
+
+int main(int argc, char **argv)
+{
+	int sock, fd;
+	int port;
+
+	if (argc != 2)
+	{
+		printf("USGE: server port\n");
+		return -1;
+	}
+	port = atoi(argv[1]);
+
+	sock = make_server_socket(port);
+	if (sock == -1)
+	{
+		return -1;
+	}
+	while (1)
+	{
+		fd = accept(sock, NULL, NULL);
+		if (fd == -1)
+		{
+			perror("Fail accept");
+			break;
+		}
+		if (process_request(fd) >= 0) //commount ok
+		{
+			break;
+		}
+		else   //wait for right connect
+		{ //none
+		}
+	}
+	close(fd);
+	return 0;
+}
+
+#define	   PACK_SIZE  16384
+static char testbuf[PACK_SIZE];
+
+int process_request(int fd)
+{
+	int byte_cnt;
+	time_t cur_timsec = 0, end_timsec = 0, tmp_tim;
+	int send_byte_tol = 0;
+	int fd_read = 0;
+	int byte_rd = 0;
+	int tmp;
+	struct stat fl_stat;
+
+	printf("have a connect\n");
+	//read client cmd
+
+	cur_timsec = time((time_t *)0);
+	printf("cur_timesec = %d\n", (int)cur_timsec);
+	//end_timsec = cur_timsec + last_tm;
+	//printf("end_timesec = %d\n", (int)end_timsec);
+	//tmp_tim = cur_timsec;
+	//memset(testbuf, '#', PACK_SIZE);  //send data ready
+	fd_read = open("/root/test.tar", O_RDONLY);
+	if (fd_read == -1)
+	{
+		perror("Fail to open file:");
+		return -1;
+	}
+
+	while (1) 
+	{
+		byte_rd = read(fd_read, testbuf, PACK_SIZE);
+		if (byte_rd == -1)
+		{
+			perror("Fail to read:");
+			break;
+		}
+		if (byte_rd == 0)
+		{
+			printf("file is over\n");
+			break;
+		}
+//		printf("%s\n", testbuf);
+		byte_cnt = write(fd, testbuf, byte_rd);
+		tmp = 0;
+		while (byte_cnt <  byte_rd)
+		{	
+			tmp += byte_cnt;
+			usleep(1);
+			//NOP();
+		
+			byte_rd = byte_rd - byte_cnt;
+			byte_cnt = write(fd, &testbuf[tmp], byte_rd);
+			if (byte_cnt < 0)
+			{
+				perror("Fail to write:");
+				break;
+			}
+		}
+		send_byte_tol += byte_cnt;
+	//	cur_timsec = time((time_t*)0);
+	
+		//if (cur_timsec-tmp_tim > 1)  //for 1 sec
+		//{
+			//output 
+		//	printf("send bytes %d\n", send_byte_tol);
+		//	tmp_tim = cur_timsec;
+		//	printf("##cur_timsec = %d\n", (int)cur_timsec);
+		//}
+		//if (cur_timsec >= end_timsec)
+	//	{
+		//	printf("cur_timsec = %d\n", (int)cur_timsec);
+		//	break;
+	//	}
+	//	usleep(1);
+	}
+	cur_timsec = time((time_t*)0);
+	printf("endtime: %d\n", (int)cur_timsec);
+	printf("Send_byte_tol = %d\n", send_byte_tol);
+	close(fd_read);
+	return 0;
+}
+
